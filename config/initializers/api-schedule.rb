4021d0dea0bae6b07b6eb73a3e04e137328efc21
Isaac Smith <isaac.smitty@gmail.com>
2019-04-10 14:21:36 +0000

    yesterday

--- config/initializers/api-schedule.rb
+++ config/initializers/api-schedule.rb
@@ -7,35 +7,35 @@ ActiveRecord::Base.connection.reset_pk_sequence!(:game)
 # #TODAY'S GAMES
 
 # #today's date
-# date = Time.now
-# today = date.strftime("%Y-%m-%d")
+date = Time.now
+today = date.strftime("%Y-%m-%d")
 
-# #NHL API endpoint => TODAY
-# nhl_url = "https://statsapi.web.nhl.com/api/v1/schedule?date=#{today}"
+#NHL API endpoint => TODAY
+nhl_url = "https://statsapi.web.nhl.com/api/v1/schedule?date=#{today}"
 
-# #RestClient get request to the NHL endpoint parsed into a JS object
-# data = JSON.parse( RestClient.get("#{nhl_url}") )
+#RestClient get request to the NHL endpoint parsed into a JS object
+data = JSON.parse( RestClient.get("#{nhl_url}") )
 
-# #Iterating through each NHL team
-#   data["dates"][0]["games"].each do |game, index|
-#     #Creating a new game in my database and assigning it's properties
-#     game = Game.new do |key|
-#       key.gameDate = today
-#       key.awayID = game["teams"]["away"]["team"]["id"]
-#       key.awayScore = game["teams"]["away"]["score"]
-#       key.awayName = game["teams"]["away"]["team"]["name"]
-#       key.homeID = game["teams"]["home"]["team"]["id"]
-#       key.homeScore = game["teams"]["home"]["score"]
-#       key.homeName = game["teams"]["home"]["team"]["name"]
-#       key.status = game["status"]["detailedState"]
-#     end
-#     #Saving games
-#     if game.save
-#       puts "saved today's game"
-#     else
-#       puts "not saved"
-#     end
-#   end
+#Iterating through each NHL team
+  data["dates"][0]["games"].each do |game, index|
+    #Creating a new game in my database and assigning it's properties
+    game = Game.new do |key|
+      key.gameDate = today
+      key.awayID = game["teams"]["away"]["team"]["id"]
+      key.awayScore = game["teams"]["away"]["score"]
+      key.awayName = game["teams"]["away"]["team"]["name"]
+      key.homeID = game["teams"]["home"]["team"]["id"]
+      key.homeScore = game["teams"]["home"]["score"]
+      key.homeName = game["teams"]["home"]["team"]["name"]
+      key.status = game["status"]["detailedState"]
+    end
+    #Saving games
+    if game.save
+      puts "saved today's game"
+    else
+      puts "not saved"
+    end
+  end
 
 
 # #YESTERDAY'S GAMES
@@ -75,32 +75,32 @@ ActiveRecord::Base.connection.reset_pk_sequence!(:game)
 # #TOMORROW'S GAMES
 
 # #Calculate tomorrow's date
-# date = Time.now + 86400
-# tomorrow = date.strftime("%Y-%m-%d")
+date = Time.now + 86400
+tomorrow = date.strftime("%Y-%m-%d")
 
-# #NHL API endpoint for yesterdays games
-# nhl_url = "https://statsapi.web.nhl.com/api/v1/schedule?date=#{tomorrow}"
+#NHL API endpoint for yesterdays games
+nhl_url = "https://statsapi.web.nhl.com/api/v1/schedule?date=#{tomorrow}"
 
-# #RestClient get request to the NHL endpoint parsed into a JS object
-# data = JSON.parse( RestClient.get("#{nhl_url}") )
+#RestClient get request to the NHL endpoint parsed into a JS object
+data = JSON.parse( RestClient.get("#{nhl_url}") )
 
-# #Iterating through each NHL team
-#   data["dates"][0]["games"].each do |game, index|
-#     #Creating a new game in my database and assigning it's properties
-#     game = Game.new do |key|
-#       key.gameDate = tomorrow
-#       key.awayID = game["teams"]["away"]["team"]["id"]
-#       key.awayScore = game["teams"]["away"]["score"]
-#       key.awayName = game["teams"]["away"]["team"]["name"]
-#       key.homeID = game["teams"]["home"]["team"]["id"]
-#       key.homeScore = game["teams"]["home"]["score"]
-#       key.homeName = game["teams"]["home"]["team"]["name"]
-#       key.status = game["status"]["detailedState"]
-#     end
-#     #Saving games
-#     if game.save
-#       puts "saved tomorrow's game"
-#     else
-#       puts "not saved"
-#     end
-#   end
+#Iterating through each NHL team
+  data["dates"][0]["games"].each do |game, index|
+    #Creating a new game in my database and assigning it's properties
+    game = Game.new do |key|
+      key.gameDate = tomorrow
+      key.awayID = game["teams"]["away"]["team"]["id"]
+      key.awayScore = game["teams"]["away"]["score"]
+      key.awayName = game["teams"]["away"]["team"]["name"]
+      key.homeID = game["teams"]["home"]["team"]["id"]
+      key.homeScore = game["teams"]["home"]["score"]
+      key.homeName = game["teams"]["home"]["team"]["name"]
+      key.status = game["status"]["detailedState"]
+    end
+    #Saving games
+    if game.save
+      puts "saved tomorrow's game"
+    else
+      puts "not saved"
+    end
+  end
